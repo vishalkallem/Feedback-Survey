df928548c0ac163660b9f204c1b81d9f7d564e31
Vishal Reddy <28930993+vishalkallem@users.noreply.github.com>
2020-01-12 01:24:26 -0800

    Added google oauth

--- Servers/index.js
+++ Servers/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('C:/Vishal/Projects/Feedback/config/keys');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-});
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('Access Token:', accessToken);
+        console.log('Refresh Token:', refreshToken);
+        console.log('Profile:', profile);
+    })
+);
 
+app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+const PORT = 5000 || process.env.PORT;
 app.listen(PORT);
