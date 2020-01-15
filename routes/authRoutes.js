b7962c66d1a2797d7f94f157fd5751a719ac3216
Vishal Reddy <28930993+vishalkallem@users.noreply.github.com>
2020-01-15 00:11:46 -0800

    Testing Authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -21,4 +21,8 @@ module.exports = app => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google", null, null));
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
