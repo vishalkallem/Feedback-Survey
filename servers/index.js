79867fc73e5bf2362d9f0d834149768c2c6ba1af
Vishal Reddy <28930993+vishalkallem@users.noreply.github.com>
2020-01-14 22:17:46 -0800

    Creates user record in the database

--- servers/index.js
+++ servers/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
+require("../models/user");
 require("../services/passport");
-require("../models/User");
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
