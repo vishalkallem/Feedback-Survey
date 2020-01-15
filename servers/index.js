650b01724e8cf0a3348b3261acbbff5bc1697cc3
Vishal Reddy <28930993+vishalkallem@users.noreply.github.com>
2020-01-15 00:04:45 -0800

    Enabled cookies

--- servers/index.js
+++ servers/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("../config/keys");
 require("../models/user");
 require("../services/passport");
@@ -10,6 +12,15 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 2592000000, //30 days = 30 * 24 hrs = 30 * 24 * 60 min = 30 * 24 * 60 * 60 sec = 30 * 24 * 60 * 60 * 1000 ms
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize(null));
+app.use(passport.session(null));
 
 require("../routes/authRoutes")(app);
 
